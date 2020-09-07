debec5899d0c5780c6b308c3c25275582d560644
nicolas.duchene <nicolas.duchene@server.mila.quebec>
2020-09-07 18:08:53 -0400

    Add timing functions

--- run.py
+++ run.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import os
 import sys
+from datetime import datetime
 from Scripts.generate_examples import text_to_tsv
 from Scripts.join_tsv_with_preds import join_tsv_with_preds
-
-
 
 def main(argv):
     input_dir = argv[0]
@@ -19,21 +18,30 @@ def main(argv):
     print("Transforming data to tsv ...\n\n\n")
     tsv_fpaths = text_to_tsv(input_dir, output_dir, question_file)
     
+    time_results = pd.DataFrame()
+
     for fpath in tsv_fpaths:
         print("\n\nGetting Predictions for %s ..."%fpath)
         output_path = fpath[:-4] + "_predictions.json"
         
         prediction_command = "allennlp predict %s %s --output-file %s --weights-file %s --include-package allennlp_ccqa_extension.dataset_readers.ccqa_datasetreader --include-package allennlp_ccqa_extension.models.basic_classifier_modified --use-dataset-reader --silent --batch-size 8" %(model_path, fpath, output_path, weights_file)
-        
+
         # use allennlp to predict
+        start_time = datetime.now()
         os.system(prediction_command)
+        end_time = datetime.now()
         print("Done predicting for  ...")
+
+        # compile timing
+        time_result = pd.DataFrame({fpath: (end_time - start_time).seconds()})
+        time_results.append(pd.DataFrame(performance_dict)
 
         print("Joining predictions output (%s) with original generated tsv (%s) ...\n\n"%(output_path, fpath))
         # join 
         join_tsv_with_preds(fpath, output_path, output_dir)
         
-        
+    time_out = output_dir.strip("/") + "/timing.pkl"
+    time_results.to_csv(time_out)
     print("All done!")
 
 
